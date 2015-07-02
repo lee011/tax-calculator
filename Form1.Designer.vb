8f1ced2c286300bd014ab191c5de795b235d1be0
Kelvin Lee <leekalok804@gmail.com>
2015-07-03 01:06:37 +0800

    Upload All Files

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "請輸入計算金額:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.TextBox1.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBox1.Location = New System.Drawing.Point(13, 29)
+        Me.TextBox1.MaxLength = 12
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(222, 39)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.TextBox2.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TextBox2.Location = New System.Drawing.Point(204, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(270, 27)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "零圓整"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(130, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 12)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "轉換大寫為:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 12)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "含稅價格:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.TextBox3.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TextBox3.Location = New System.Drawing.Point(12, 128)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(222, 39)
+        Me.TextBox3.TabIndex = 1
+        Me.TextBox3.Text = "0"
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 12)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "含稅價格之稅金:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.TextBox4.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TextBox4.Location = New System.Drawing.Point(12, 185)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(222, 39)
+        Me.TextBox4.TabIndex = 1
+        Me.TextBox4.Text = "0"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(253, 113)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 12)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "除稅價格:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(253, 170)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 12)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "除稅價格之稅金:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.TextBox5.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TextBox5.Location = New System.Drawing.Point(252, 128)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(222, 39)
+        Me.TextBox5.TabIndex = 1
+        Me.TextBox5.Text = "0"
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.TextBox6.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TextBox6.Location = New System.Drawing.Point(252, 185)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(222, 39)
+        Me.TextBox6.TabIndex = 1
+        Me.TextBox6.Text = "0"
+        Me.TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(13, 80)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(52, 12)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "稅率 (%):"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(71, 78)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(53, 22)
+        Me.NumericUpDown1.TabIndex = 2
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(242, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 39)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "清除"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(492, 242)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "含稅除稅價格計算器"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode >= Keys.D0 And e.KeyCode <= Keys.D9 Then
+            e.SuppressKeyPress = False
+        ElseIf e.KeyCode >= Keys.NumPad0 And e.KeyCode <= Keys.NumPad9 Then
+            e.SuppressKeyPress = False
+        ElseIf e.KeyCode = Keys.Back Then
+            e.SuppressKeyPress = False
+        Else
+            Select Case e.KeyCode
+                Case Keys.Left, Keys.Up, Keys.Down, Keys.Right, Keys.Home, Keys.End
+                    e.SuppressKeyPress = False
+                Case Else
+                    My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Beep)
+                    e.SuppressKeyPress = True
+            End Select
+        End If
+    End Sub
+
+    Private Function ConvertToChineseNumbers(inputNumber As Long) As String
+
+        Dim s As String = String.Format("{0:0}", inputNumber)
+        Dim chinNums As String = "零壹貳參肆伍陸柒捌玖"
+        Dim chinNums1 As String = "萬億兆"
+        Dim chinNums2 As String = "拾佰仟"
+        Dim output As String = ""
+        Dim i As Long = 0
+        Dim j As Long = -1
+        Dim k As Long = -1
+        Dim z As Long = s.Length - 1
+        If s = "0" Then
+            output &= "零"
+        Else
+            While z >= 0 AndAlso s(z) = "0"c
+                z -= 1
+            End While
+            For i = 0 To z
+                Dim x As Long = (s.Length - 1 - i) Mod 4
+                Dim y As Long = (s.Length - 1 - i) \ 4
+                output &= chinNums(Convert.ToInt64(s(i).ToString))
+                If x = 0 And y = 0 Then
+                ElseIf x = 0 Then
+                    output &= chinNums1(y - 1)
+                Else
+                    If s(i) <> "0"c Then
+                        output &= chinNums2(x - 1)
+                    End If
+                End If
+            Next
+            If s.Length - 1 - z >= 4 Then
+                Dim y As Long = (s.Length - 1 - z) \ 4
+                output &= chinNums1(y - 1)
+            End If
+        End If
+
+        output &= "圓整"
+
+        While output.IndexOf("零零") <> -1
+            output = output.Replace("零零", "零")
+        End While
+        For Each inoo As String In chinNums1
+            While output.IndexOf(inoo & inoo) <> -1
+                output = output.Replace(inoo & inoo, inoo)
+            End While
+        Next
+        For Each inoo As String In chinNums1
+            output = output.Replace("零" & inoo, inoo)
+        Next
+        output = output.Replace("億萬", "億")
+        output = output.Replace("兆億", "兆")
+        Return output
+    End Function
+
+    Private Function GetPriceWithTax(inputNumber As Long, taxRate As Double, ByRef taxValue As Long) As Long
+        Dim r As Double = inputNumber * (1 + taxRate)
+        taxValue = inputNumber * taxRate
+        Return r
+    End Function
+
+    Private Function GetPriceWithoutTax(inputNumber As Long, taxRate As Double, ByRef taxValue As Long) As Long
+        Dim r As Double = inputNumber / (1 + taxRate)
+        taxValue = inputNumber - r
+        Return r
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then
+        ElseIf TextBox1.Text.Length > 1 AndAlso TextBox1.Text.StartsWith("0") Then
+            TextBox1.Text = TextBox1.Text.TrimStart("0")
+        End If
+        Dim inputVal As Long = If(TextBox1.Text = "", 0, Convert.ToInt64(TextBox1.Text))
+        If inputVal > 999999999999 Then
+            Return
+        End If
+        TextBox2.Text = ConvertToChineseNumbers(inputVal)
+        Dim tv As Long = 0.0
+        TextBox3.Text = String.Format("{0:0}", GetPriceWithTax(inputVal, NumericUpDown1.Value / 100, tv))
+        TextBox4.Text = String.Format("{0:0}", tv)
+        TextBox5.Text = String.Format("{0:0}", GetPriceWithoutTax(inputVal, NumericUpDown1.Value / 100, tv))
+        TextBox6.Text = String.Format("{0:0}", tv)
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim inputVal As Long = Convert.ToInt64(TextBox1.Text)
+        Dim tv As Long = 0.0
+        TextBox3.Text = String.Format("{0:0}", GetPriceWithTax(inputVal, NumericUpDown1.Value / 100, tv))
+        TextBox4.Text = String.Format("{0:0}", tv)
+        TextBox5.Text = String.Format("{0:0}", GetPriceWithoutTax(inputVal, NumericUpDown1.Value / 100, tv))
+        TextBox6.Text = String.Format("{0:0}", tv)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox1.Focus()
+    End Sub
+End Class
